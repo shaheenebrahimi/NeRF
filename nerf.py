a6ca1a526f1f74ad45d2597f0c15a78aba72be99
Connor Temple <connor.temple308@gmail.com>
2024-04-14 15:48:13 -0500

    added render function

--- nerf.py
+++ nerf.py
@@ -1,5 +1,5 @@
 from load_data import *
-import keras
+import tensorflow.keras as keras
 import tensorflow as tf
 import numpy as np
 
@@ -53,6 +53,51 @@ class NeRF(keras.Model):
 
     def render(self, ray_origins, ray_directions):
         '''Render image based on camera ray origins and ray directions'''
+
+def accumulated_transmittance(alphas):
+    transmittance = tf.math.cumprod(alphas, axis=1, exclusive=True)
+    ones = tf.ones([tf.shape(transmittance)[0], 1], dtype = alphas.dtype)
+    return tf.concat([ones, transmittance[:, :-1]], axis=-1)
+
+
+def render(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
+    
+    # Sample points along rays, linear space between hn and hf
+    t = tf.tile(tf.linspace(hn, hf, nb_bins)[None, :], [ray_origins.shape[0], 1]) # expand to match the number of rays
+    # t = tf.repeat(tf.linspace(hn, hf, nb_bins)[None, :], ray_origins.shape[0], axis=0) # effectively equivalent to above
+
+    # Random perturbation
+    mid = (t[:, :-1] + t[:, 1:]) / 2.
+    lower = tf.concat([t[:, :1], mid], axis=-1)
+    upper = tf.concat([mid, t[:, -1:]], axis=-1)
+    u = tf.random.uniform(tf.shape(t))
+    t = lower + (upper - lower) * u 
+
+    # Delta for transmittance calculation
+    const = tf.repeat(tf.constant([1e10])[None, :], ray_origins.shape[0], axis=0)
+    delta = tf.concat([t[:, 1:] - t[:, :-1], const], axis=-1)
+
+    # Compute 3D points along each ray
+    # x = tf.expand_dims(ray_origins, 1) + tf.expand_dims(t, 2) * tf.expand_dims(ray_directions, 1) # in case below doesn't work
+    x = ray_origins[:, None, :] + t[:, :, None] * ray_directions[:, None, :]
+    ray_dirs = tf.repeat(ray_directions[:, None, :], nb_bins, axis=1)
+
+    # Model Prediction
+    colors, sigma = nerf_model([tf.reshape(x, [-1, 3]), tf.reshape(ray_dirs, [-1, 3])])
+    colors = tf.reshape(colors, tf.shape(x))
+    sigma = tf.reshape(sigma, tf.shape(x)[:-1])
+
+    # Compute alpha and weights
+    alpha = 1 - tf.exp(-sigma * delta)
+    transmittance = accumulated_transmittance(1 - alpha)
+    weights = tf.expand_dims(transmittance, 2) * tf.expand_dims(alpha, 2)
+
+    # Compute final color as weighted sum of colors along each ray
+    c = tf.reduce_sum(weights * colors, axis=1)
+    weight_sum = tf.reduce_sum(tf.reduce_sum(weights, axis=-1), axis=-1) # regularization of white background
+
+    return c + (1 - tf.expand_dims(weight_sum, -1))
+
 
 if __name__ == '__main__':
     model = NeRF()
