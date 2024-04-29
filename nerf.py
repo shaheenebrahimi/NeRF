1b366107bf911653c3fc67a219b913906e484cba
Shaheen-Ebrahimi <shaheenebrahimi@tamu.edu>
2024-04-29 11:19:02 -0500

    Add gen image every 10 epochs

--- nerf.py
+++ nerf.py
@@ -34,15 +34,6 @@ class NeRF(keras.Model):
             keras.layers.Dense(3, activation='sigmoid')
         ])
 
-    @staticmethod
-    def positional_encoding(pos, L):
-        '''Encode ray to sinusoidal harmonics'''
-        result = []
-        for i in range(L//2):
-            result.append(tf.sin(2**i * pos))
-            result.append(tf.cos(2**i * pos))
-        return tf.concat(result, axis=-1)
-    
     def get_config(self):
         config = super(NeRF, self).get_config()
         config.update({'embedded_x_dim': self.embedded_x_dim,
@@ -52,6 +43,15 @@ class NeRF(keras.Model):
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+    @staticmethod
+    def positional_encoding(pos, L):
+        '''Encode ray to sinusoidal harmonics'''
+        result = []
+        for i in range(L//2):
+            result.append(tf.sin(2**i * pos))
+            result.append(tf.cos(2**i * pos))
+        return tf.concat(result, axis=-1)
     
     def call(self, x, d):
         '''Forward propagation call'''
@@ -71,10 +71,10 @@ def accumulated_transmittance(alphas):
     return tf.concat([ones, transmittance[:, :-1]], axis=-1)
 
 
-def render(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
+def render(_model, _ray_origins, _ray_directions, hn=0, hf=0.5, nb_bins=192):
     '''Render image based on camera ray origins and ray directions'''
     # Sample points along rays, linear space between hn and hf
-    t = tf.cast(tf.tile(tf.linspace(hn, hf, nb_bins)[None, :], [ray_origins.shape[0], 1]), dtype=tf.dtypes.float32) # expand to match the number of rays
+    t = tf.cast(tf.tile(tf.linspace(hn, hf, nb_bins)[None, :], [_ray_origins.shape[0], 1]), dtype=tf.dtypes.float32) # expand to match the number of rays
     
     # Get lower and upper bounds for each bin
     mid = (t[:, :-1] + t[:, 1:]) / 2.
@@ -84,15 +84,15 @@ def render(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
     t = lower + (upper - lower) * u # parametric representation
 
     # Delta for transmittance calculation
-    const = tf.repeat(tf.constant([1e10], dtype=tf.dtypes.float32)[None, :], ray_origins.shape[0], axis=0)
+    const = tf.repeat(tf.constant([1e10], dtype=tf.dtypes.float32)[None, :], _ray_origins.shape[0], axis=0)
     delta = tf.concat([t[:, 1:] - t[:, :-1], const], axis=-1)
 
     # Compute 3D points along each ray
-    x = ray_origins[:, None, :] + t[:, :, None] * ray_directions[:, None, :] # sampled point
-    ray_directions = tf.repeat(ray_directions[:, None, :], nb_bins, axis=1) # resize for batching
+    x = _ray_origins[:, None, :] + t[:, :, None] * _ray_directions[:, None, :] # sampled point
+    ray_directions = tf.repeat(_ray_directions[:, None, :], nb_bins, axis=1) # resize for batching
 
     # Model Prediction
-    colors, sigma = nerf_model(tf.reshape(x, [-1, 3]), tf.reshape(ray_directions, [-1, 3]))
+    colors, sigma = _model(tf.reshape(x, [-1, 3]), tf.reshape(ray_directions, [-1, 3]))
     colors = tf.reshape(colors, tf.shape(x))
     sigma = tf.reshape(sigma, tf.shape(x)[:-1])
 
@@ -107,38 +107,40 @@ def render(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
 
     return c + (1 - tf.expand_dims(weight_sum, -1))
 
-def train(nerf_model, optimizer, data, epochs=int(1e5), hn=0, hf=1, nb_bins=192, H=400, W=400):
+def fit(_model, _optimizer, _train_data, _test_data, epochs=int(1e5), hn=0, hf=1, nb_bins=192, H=400, W=400):
     # Clear log file
     with open('logs/log.txt', 'w') as log_file:
         log_file.write('')
 
     training_loss = []
     for i in range(epochs):
-        for batch in tqdm(data, desc='Epoch ' + str(i), ncols=100):
+        for batch in tqdm(_train_data, desc='Epoch ' + str(i), ncols=100):
             ray_origins = batch[:, :3]
             ray_directions = batch[:, 3:6]
             y = batch[:, 6:] # ground truth
 
             # Record forward pass with tape
             with tf.GradientTape() as tape:
-                logits = render(nerf_model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins)
+                logits = render(_model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins)
                 loss = tf.math.reduce_sum((y - logits) ** 2)
             
             # Compute and apply gradients
-            grads = tape.gradient(loss, nerf_model.trainable_weights)
-            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+            grads = tape.gradient(loss, _model.trainable_weights)
+            _optimizer.apply_gradients(zip(grads, _model.trainable_weights))
 
             # Output loss to logfile
             with open('log.txt', 'a') as log_file:
                 log_file.write(str(loss.numpy()) + '\n')
         
+        # Save model and test every 10 epochs
         training_loss.append(loss)
-        model.save('nuthin.keras')
+        _model.save('nuthin.keras')
+        if i % 10 == 0: gen_image(_model, _test_data, version=i, hn=hn, hf=hf, nb_bins=nb_bins, H=H, W=W, chunk_size=5)
 
     log_file.close()
     return training_loss
 
-def test(nerf_model, hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
+def gen_image(_model, _dataset, version=0, hn=0, hf=1, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
     """
     Args:
         hn: near plane distance
@@ -153,22 +155,22 @@ def test(nerf_model, hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H
     Returns:
         None: None
     """
-    ray_origins = dataset[img_index * H * W: (img_index + 1) * H * W, :3]
-    ray_directions = dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
+    ray_origins = _dataset[img_index * H * W: (img_index + 1) * H * W, :3]
+    ray_directions = _dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
 
     data = [] # list of regenerated pixel values
     for i in range(int(np.ceil(H / chunk_size))):   # iterate over chunks
         # Get chunk of rays
         o = ray_origins[i * W * chunk_size: (i + 1) * W * chunk_size]
         d = ray_directions[i * W * chunk_size: (i + 1) * W * chunk_size]   
-        generated_px_values = render(nerf_model, ray_origins=o, ray_directions=d, hn=hn, hf=hf, nb_bins=nb_bins)
+        generated_px_values = render(_model, ray_origins=o, ray_directions=d, hn=hn, hf=hf, nb_bins=nb_bins)
         data.append(generated_px_values)
     img = np.array(data).reshape(H, W, 3)
 
     plt.figure()
     # plt.imshow(img)
     # plt.savefig(f'novel_views/img_{img_index}.png', bbox_inches='tight')
-    plt.imsave(f'novel_views/img_{img_index}.png', img)
+    plt.imsave(f'novel_views/img_{img_index}_{version}.png', img)
     plt.close()
 
 if __name__ == '__main__':
@@ -186,11 +188,12 @@ if __name__ == '__main__':
     
     # Create and train model
     model = NeRF()
+    fit(model, tf.optimizers.legacy.Adam(LEARNING_RATE), train_dataset, test_dataset)
     # train(model, tf.optimizers.legacy.Adam(LEARNING_RATE), train_dataset, epochs=EPOCHS)
     #model = keras.models.load_model('nuthin1.keras', custom_objects={"NeRF": NeRF})
     #print(model.summary())
     # train(model, tf.optimizers.legacy.Adam(LEARNING_RATE), train_dataset, epochs=EPOCHS)
     #train(model, keras.optimizers.Adam(LEARNING_RATE), train_dataset, epochs=EPOCHS)
-    test(model,0,1,test_dataset)
+    # test(model,0,1,test_dataset)
 
 
